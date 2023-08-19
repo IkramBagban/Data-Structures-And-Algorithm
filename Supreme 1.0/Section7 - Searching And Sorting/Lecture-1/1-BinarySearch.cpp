809009d562d6c79bd8f82766eae35bf92568f92b
ikramBagban <ikrambagban471gmail.com>
2023-08-20 01:47:22 +0530

    Update Binary Search - Added explanatory comments and Notes

--- Supreme 1.0/Section7 - Searching And Sorting/Lecture-1/1-BinarySearch.cpp
+++ Supreme 1.0/Section7 - Searching And Sorting/Lecture-1/1-BinarySearch.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm> // Include the algorithm library for binary_search
+#include <vector>
 using namespace std;
 
 // Notes:
@@ -12,8 +14,8 @@ using namespace std;
 // Function to perform binary search
 int BinarySearch(int arr[], int target, int size)
 {
-    int start = 0;              // Starting index of the search interval
-    int end = size - 1;         // Ending index of the search interval
+    int start = 0;      // Starting index of the search interval
+    int end = size - 1; // Ending index of the search interval
 
     int mid = (start + end) / 2; // Calculate the midpoint of the Selected range of array
 
@@ -59,6 +61,51 @@ int main()
     else
     {
         cout << "Target Element Found At index " << indexOfTarget << endl;
+    }
+
+    // ---------------------------------------------------------------------
+    // ---------------------------------------------------------------------
+    // Let's explore the "binary_search" function from the STL
+    // Using the built-in binary_search from the STL. To use this, we have to include the algorithm library in the header file.
+
+    // The binary_search function from the STL is used to search for a specified value within a sorted container.
+    // It takes three parameters:
+    // 1. The iterator to the beginning of the container (inclusive).
+    // 2. The iterator to the end of the container (exclusive).
+    // 3. The value that is being searched for in the container.
+
+    // If the specified value is found within the container, the function returns true.
+    // Otherwise, if the value is not found, it returns false.
+
+    // This function is commonly used with sorted sequences like arrays and vectors to quickly determine the presence of a value.
+    // It operates on containers of any data type as long as they support comparison with the less-than operator (<).
+
+    // Example usage:
+    vector<int> v{2, 4, 6, 7, 8, 10};
+    if (binary_search(v.begin(), v.end(), 16)) // searching on vector
+    {
+        cout << "found " << endl;
+    }
+    else
+    {
+        cout << "Not Found " << endl;
+    }
+
+    system("color 0A");
+
+
+    // -----
+    int arr2[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    // Example usage of binary_search in a sorted array
+    int arr2target = 6;
+    int arrSize = 8;
+    if (binary_search(arr2, arr2 + arrSize, target))
+    {
+        cout << "Value " << arr2target << " found in the array." << endl;
+    }
+    else
+    {
+        cout << "Value " << arr2target << " not found in the array." << endl;
     }
     return 0;
 }
