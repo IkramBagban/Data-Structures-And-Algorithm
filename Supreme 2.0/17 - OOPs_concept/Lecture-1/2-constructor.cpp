dc38489a432c498c7d0949becd116f2e297d3f8b
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-10-24 14:09:22 +0530

    Class, Object, Properties, Behaviour, AccessModifier, Padding

---
+++ Supreme 2.0/17 - OOPs_concept/Lecture-1/2-constructor.cpp
@@ -0,0 +1,108 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    bool gf; // Private member variable indicating whether the student has a girlfriend
+
+public:
+    string name; // Public member variable for student's name
+    int roll_no; // Public member variable for student's roll number
+    int age;     // Public member variable for student's age
+
+    // Constructors : Constructors are special member functions of a class.They are executed whenever new objects of that class are created.They have the same name as the class and don't return any type, not even void.
+
+    // Default Constructor:
+    // A default constructor is a constructor that either has no parameters or
+    // if it has parameters, all the parameters have default values.
+    // It gets automatically called when objects of the class are declared but not initialized.
+    Student()
+    {
+        cout << "Constructor: Default constructor invoked." << endl;
+    }
+
+    // Parameterized Constructor : It is a constructor that accepts one or more parameters
+    // A parameterized constructor is a constructor that takes parameters,
+    // and is used to provide different values to distinct objects.
+    // This will be called when an object of the class is declared and initialized
+    // using the specified parameters.
+    Student(string _name, int _roll_no, int _age, bool _gf)
+    {
+        cout << "Constructor: Parameterized constructor invoked (without using 'this' keyword)." << endl;
+        name = _name; // Assigning values to the class members
+        roll_no = _roll_no;
+        age = _age;
+        gf = _gf;
+    }
+
+    // Another Parameterized Constructor with a different set of parameters.
+    // The 'this' keyword in C++ is a pointer that points to the object which
+    // invokes the member function (or constructor). Here, 'this' is used
+    // because the parameter names are same as the class member names.
+    Student(string name, int roll_no, int age)
+    {
+        cout << "Constructor: Parameterized constructor invoked (using 'this' keyword for clarity)." << endl;
+        this->name = name;       // 'this->name' refers to the class member 'name'
+        this->roll_no = roll_no; // 'this->roll_no' refers to the class member 'roll_no'
+        this->age = age;         // 'this->age' refers to the class member 'age'
+    }
+};
+
+int main()
+{
+    // this will call the default constructor
+    Student s1;
+    s1.name = "Urhan";
+    s1.roll_no = 12;
+    s1.age = 19;
+
+    cout << "Name Of s1 Student is " << s1.name << endl;
+    cout << "rollNo. Of s1 Student is " << s1.roll_no << endl;
+    cout << "age Of s1 Student is " << s1.age << endl;
+
+    cout << endl;
+
+    // Creating s2 Student object using the parameterized constructor
+    Student s2("ikram", 13, false, 19);
+    cout << "Name Of S2 Student is " << s2.name << endl;
+    cout << "rollNo. Of S2 Student is " << s2.roll_no << endl;
+    cout << "age Of s2 Student is " << s2.age << endl;
+
+    // Note: can't access 'gf' name because 'gf' is private.
+    // cout << "is s2 student has gf " << s2.gf<< endl;
+
+    cout << endl;
+
+    // Creating s3 Student object using the parameterized constructor
+    Student s3("Bushrah", 14, 18);
+    cout << "Name Of S3 Student is " << s3.name << endl;
+    cout << "rollNo. Of S3 Student is " << s3.roll_no << endl;
+    cout << "age Of S3 Student is " << s3.age << endl;
+
+    cout << endl;
+
+    // Creating an object s4 of the Student class using a parameterized constructor.
+    // We're allocating it on the heap, which means s4 will give us the base address.
+    Student *s4 = new Student("Muskan", 15, 18);
+    // When we allocate an object on the heap, s4 is a pointer that holds the base address.
+    // We can access the object's properties using the '->' operator.
+
+    // Let's print the base address of s4.
+    cout << "Base Address Of S4 Student is " << s4 << endl;
+
+    // To access the properties of the Student object, we use '->' notation.
+    cout << "Name Of S4 Student is " << s4->name << endl;
+    cout << "Roll Number Of S4 Student is " << s4->roll_no << endl;
+    cout << "Age Of S4 Student is " << s4->age << endl;
+
+    // If you prefer, you can also access properties using the '*' operator and dot notation.
+    // This is equivalent to using '->'.
+    cout << "Another Way to Access Roll Number Of S4 Student is " << (*s4).roll_no << endl;
+
+    return 0;
+}
---
+++ Supreme 2.0/17 - OOPs_concept/Lecture-1/tempCodeRunnerFile.cpp
@@ -0,0 +1,87 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+#include <iostream>
+using namespace std;
+
+// Class Definition:
+// A class in C++ is a user-defined type or data structure declared with keyword class
+// that has data and functions (also called member variables and member functions) 
+// as its members whose access is governed by the associated access specifiers.
+class Student
+{
+    // Access Modifiers:
+    // private: This keyword is an access specifier. Data members and member functions 
+    // declared under 'private' are not accessible outside of the class. Only member functions 
+    // of that class can access the private data members and member functions.
+    private:
+        bool gf;  // Represents whether the student has a girlfriend or not.
+
+    // public: Member variables or member functions declared under 'public' are accessible 
+    // from outside the class.
+    public:
+        string name;  // Name of the student
+        int roll_no;  // Roll number of the student
+        int age;      // Age of the student
+        int grade;    // Grade of the student
+
+    // Member Function (or Behavior):
+    // A member function of a class is a function that has its definition or its 
+    // prototype within the class definition similar to any other variable. 
+    void study()
+    {
+        cout << name << " Is Studying" << endl;
+    }
+};
+
+
+
+int main()
+{
+    // Object Creation:
+    // An object is an instance of a class. When a class is defined, only the specification 
+    // for the object is defined; no memory or storage is allocated for it. Here, 's1' is an object of the 'Student' class.
+    Student s1;
+
+
+    // Assigning values to public members of the object:
+    s1.name = "Ikram";
+    s1.roll_no = 13;  // Let's say roll number is 13
+    s1.age = 19;       // Let's say age is 19
+    s1.grade = 12;     // Let's say grade is 3rd Year
+
+    // Printing the values of the properties:
+    cout << "Name Of the s1 Student is " << s1.name << endl;
+    cout << "Roll No. of the s1 Student is " << s1.roll_no << endl;
+    cout << "Age of the s1 Student is " << s1.age << " years" << endl;
+    cout << "Grade of the s1 Student is " << s1.grade << "th" << endl;
+
+    // Size of Class and Padding:
+    // Memory in computers is organized in a hierarchical manner, with the smallest addressable unit being a byte.
+    // However, for performance reasons, the computer's hardware (specifically, the memory subsystem) 
+    // often reads memory in chunks larger than a byte, like words or double words.
+    //
+    // To ensure that these chunks can be read efficiently, the data should be aligned in memory 
+    // according to its size. For example, a 4-byte integer should start at a memory address that's a 
+    // multiple of 4. This is where padding comes in.
+    //
+    // Consider a simple class:
+    //
+    // class Example {
+    //     char c;  // 1 byte
+    //     int i;  // 4 bytes
+    // };
+    //
+    // In this example, after the char 'c', 3 bytes of padding might be added before 'i' to ensure 
+    // that 'i' starts at a memory address that's a multiple of 4. This would make the size of the 'Example' 
+    // class not 5, but 8 bytes. Note that padding might not always be added, and it's platform/compiler-specific.
+    //
+    // In the case of the 'Student' class, similar padding might be added due to the data types used, 
+    // making its size potentially larger than the simple sum of its member sizes.
+    cout << "Size Of The Class In Byte Is " << sizeof(s1) << endl;
+
+    
+
+
+    return 0;
+}
