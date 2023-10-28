1cd99a0cacb045bd980eb45886ab8aeb95465b35
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-10-28 18:44:26 +0530

    Type Of Inheritance

---
+++ Supreme 2.0/17 - OOPs_concept/Lecture-2/Inheritance/4-singleInheritance.cpp
@@ -0,0 +1,63 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+
+#include <iostream>
+using namespace std;
+
+// Base class definition
+class Base
+{
+public:
+    int a;
+    int b;
+
+    // Parameterized constructor of the Base class
+    Base(int a, int b)
+        : a(a), b(b) // Initializer list to initialize the member variables
+    {
+        cout << "Base Constructor Called" << endl;
+    }
+
+    // A method to print the values of a and b
+    void print()
+    {
+        cout << "a: " << a << ", b:" << b << endl;
+    }
+};
+
+// Child class that inherits from the Base class
+class Child : public Base
+{
+public:
+    // Constructor of the Child class
+    Child(int a, int b)
+        : Base(a, b) // Calling the Base class's constructor from the Child's constructor
+                     // This is crucial because Child class doesn't have its own 'a' and 'b'.
+                     // They are inherited from Base. So, to initialize them, we need
+                     // to call the Base class's constructor.
+    {
+        cout << "Child Constructor Called" << endl;
+    }
+
+    // A method specific to the Child class
+    void childMethod()
+    {
+        cout << "Child Method Called" << endl;
+    }
+};
+
+int main()
+{
+    // When we create an object of the Child class, it will first
+    // call the Base class's constructor (because of the call in the Child's constructor)
+    // and then the Child class's constructor.
+    // We can access base class public and protected methods, and properties in derived class.
+    Child c1(3, 4);
+    c1.print();
+
+    Base b1(2, 4);
+    b1.print();
+
+    return 0;
+}
