69856873ce642eced8fc048312e648c8a2d2b733
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-07 17:42:52 +0530

    Find Starting Point Of A cycle(loop) Linkedlist

--- Supreme 2.0/18-LinkedLists/Lecture-3/5-startingPointOfLoop.cpp
+++ Supreme 2.0/18-LinkedLists/Lecture-3/5-startingPointOfLoop.cpp
@@ -40,46 +40,57 @@ void print(ListNode *head)
     cout << endl;
 }
 
+// Function to create a cycle in the linked list. 
+// The cycle is created by connecting the tail to a node at the given position.
 void createCycle(ListNode *head, ListNode *tail, int position)
 {
     ListNode *temp = head;
 
+    // Traverse the list until we reach the specified position
     while (position != 1)
     {
-        temp = temp->next;
+        temp = temp->next; 
         position--;
     }
 
+    // Connect the tail's next pointer to the node at the specified position
     tail->next = temp;
 }
 
+// Function to find the starting point of the cycle in the linked list
 ListNode *findStartingPoint(ListNode *head)
 {
+    // Initialize two pointers for the cycle detection phase
     ListNode *slow = head;
     ListNode *fast = head;
 
-    // bool isCycle = false;
-
+    // Loop to find the meeting point, which confirms a cycle exists
     while (fast != NULL && fast->next != NULL)
     {
-        fast = fast->next->next;
-        slow = slow->next;
+        fast = fast->next->next; // Move fast pointer two steps ahead
+        slow = slow->next;       // Move slow pointer one step ahead
 
+        // Check if both pointers meet at the same node
         if (slow == fast)
         {
-            // isCycle = true;
-            break;
+            break; // A cycle has been detected
         }
     }
 
+    // Reset the slow pointer to the head for the next phase
     slow = head;
+
+    // Loop to find the starting point of the cycle
     while (slow != fast)
     {
+        // Move both pointers at the same pace
         slow = slow->next;
         fast = fast->next;
     }
 
-    return slow;
+    // Both pointers are now at the starting point of the cycle
+    // Returning either slow or fast will give the starting node of the cycle
+    return fast;
 }
 
 int main()
@@ -89,18 +100,22 @@ int main()
 
     insertAtHead(head, tail, 50);
     insertAtHead(head, tail, 40);
-    insertAtHead(head, tail, 30); // at third position
+    insertAtHead(head, tail, 30); // This node will be at the third position
     insertAtHead(head, tail, 20);
     insertAtHead(head, tail, 10);
+
     print(head);
 
-    // 10 k next pointer ko 30 wali node per point karwayenge to ek cycle loop bam jayenga.
-    createCycle(head, tail, 3); // after this line. the loop has been created. if we print it. it will go infinite
-    // print(head); // if we uncomment this. loop infinite time chalenga.
+    // Create a cycle in the list. The tail node will point to the node at position 3
+    createCycle(head, tail, 3); // After this line, a loop (cycle) is created.
 
-    // let's find starting point
-    ListNode *startinPointOfCycle = findStartingPoint(head);
-    cout << "Starting Point Of Cycle" << startinPointOfCycle->val << endl;
+    // Print function is commented out to avoid infinite loop due to the cycle
+    // print(head); // WARNING: This will cause an infinite loop!
+
+    // Find the starting point of the cycle in the linked list
+    ListNode *startingPointOfCycle = findStartingPoint(head);
+
+    cout << "Starting Point Of Cycle: " << startingPointOfCycle->val << endl;
 
     return 0;
 }
