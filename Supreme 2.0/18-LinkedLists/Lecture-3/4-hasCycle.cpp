cd2b7fe381c06fd545761f5da5d137e30f8f9e2a
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-07 16:40:00 +0530

    2 Approaches To solve hasCycle. 1) hashmap 2) tortoiz and hare

---
+++ Supreme 2.0/18-LinkedLists/Lecture-3/4-hasCycle.cpp
@@ -0,0 +1,63 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+// Question Link : https://leetcode.com/problems/linked-list-cycle/description/
+
+
+// Approach 1: Hash Map
+// Note: This is not the most efficient code in terms of space complexity.
+// An interviewer will likely prefer a more space-efficient approach, such as Floyd's Cycle Detection Algorithm.
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // Create a map to store the visit status of each node, default is false.
+        map<ListNode*, bool> visitedNodes;
+
+        // Start iterating from the head of the list.
+        ListNode* temp = head;
+
+        // Loop until we reach the end of the list.
+        while(temp != NULL){
+            // Check if the current node has been visited before.
+            if(visitedNodes[temp] == false){
+                // Mark the current node as visited.
+                visitedNodes[temp] = true;
+                // Move to the next node in the list.
+                temp = temp->next;
+            } else {
+                // If the current node was visited already, we found a cycle.
+                return true;
+            }
+        }
+        // If we exit the loop normally, there is no cycle.
+        return false;
+    }
+};
+
+
+// Approach 2: Floyd's Cycle Detection Algorithm (Tortoise and Hare)
+// Most Optimized Approach. The space complexity of this approach is O(1).
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // Initialize two pointers, slow (the tortoise) and fast (the hare).
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // Loop until fast pointer reaches the end of the list
+        while(fast != NULL && fast->next != NULL){
+            // Move the fast pointer two steps ahead
+            fast = fast->next->next;
+            // Move the slow pointer one step ahead
+            slow = slow->next;
+
+            // If the fast pointer and the slow pointer meet, a cycle exists
+            if(fast == slow){
+                return true;
+            }
+        }
+
+        // If the fast pointer reaches the end of the list, no cycle exists
+        return false;
+    }
+};
