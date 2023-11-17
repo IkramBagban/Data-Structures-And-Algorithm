3e3d17c0a94fd59a4d08f75054869a6d80b7e0c5
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-17 17:47:08 +0530

    Valid Parentheses

---
+++ Supreme 2.0/20-Stack/Lecture-2/2-validParentheses.cpp
@@ -0,0 +1,27 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(int i = 0; i < s.length(); i++){
+          
+            if(!st.empty() && st.top() == '(' && s[i] == ')' ){
+                st.pop();
+            }
+            else if(!st.empty() && st.top() == '[' && s[i] == ']' ){
+                st.pop();
+            }
+            else if(!st.empty() && st.top() == '{' && s[i] == '}' ){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        if(st.size()>0) return false;
+        return true;
+    }
+};
