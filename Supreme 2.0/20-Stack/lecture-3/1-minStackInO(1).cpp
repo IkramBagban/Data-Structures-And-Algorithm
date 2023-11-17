24989f49db20c4ee6b1aca7a82a0e3bbd080d93a
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-17 20:44:27 +0530

    Min Stack In O(1) TC (LEETCODE)

--- Supreme 2.0/20-Stack/lecture-3/1-minStackInO(1).cpp
+++ Supreme 2.0/20-Stack/lecture-3/1-minStackInO(1).cpp
@@ -5,35 +5,48 @@
 
 class MinStack {
 public:
+    // The stack is implemented using a vector of pairs.
+    // Each pair holds the value and the current minimum value at that point in the stack.
     vector<pair<int,int> >st;
+
     MinStack() {
         
     }
     
+    // Function to push a value onto the stack.
     void push(int val) {
+        // If the stack is empty, push the value and set it as the current minimum.
         if(st.empty()){
             pair<int,int> p;
-            p.first = val;
-            p.second = val;
+            p.first = val; // The value being pushed
+            p.second = val; // The minimum value is the value itself as the stack is empty
             st.push_back(p);
-        }else{
+        } else {
+            // If the stack is not empty, push the value and update the current minimum.
             pair<int,int> p;
-            p.first = val;
-            int prevMin = st.back().second;
-            p.second = min(p.first, prevMin);
+            p.first = val; // The value being pushed
+            int prevMin = st.back().second; // Get the previous minimum value from the stack
+            p.second = min(p.first, prevMin); // The minimum value is the lesser of the new value and the previous minimum
             st.push_back(p);
         }
     }
     
+    // Function to pop the top element from the stack.
     void pop() {
+        // Remove the top element from the stack.
+        // The pair being removed contains the value and the minimum value until that point.
         st.pop_back();
     }
     
+    // Function to get the top element of the stack.
     int top() {
-        return st.back().first;
+        // Return the first element of the pair at the top of the stack, which is the value.
+        return st.back().first; 
     }
     
+    // Function to get the minimum element in the stack.
     int getMin() {
+        // Return the second element of the pair at the top of the stack, which is the current minimum value.
         return st.back().second;
     }
 };
