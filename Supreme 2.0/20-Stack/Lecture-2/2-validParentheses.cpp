1819cc9b3782cd5d3f78db62fcc01fb776bbf448
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-11-17 17:47:57 +0530

    Valid Parantheses (Leetcode)

--- Supreme 2.0/20-Stack/Lecture-2/2-validParentheses.cpp
+++ Supreme 2.0/20-Stack/Lecture-2/2-validParentheses.cpp
@@ -1,27 +1,38 @@
 // Author: Ikram Bagban
 // Email: bagbanikram@gmail.com
 // GitHub: https://github.com/ikrambagban
+// Question Link : https://leetcode.com/problems/valid-parentheses/
+// (Leetcode)
 
-class Solution {
+class Solution
+{
 public:
-    bool isValid(string s) {
+    bool isValid(string s)
+    {
         stack<char> st;
-        
-        for(int i = 0; i < s.length(); i++){
-          
-            if(!st.empty() && st.top() == '(' && s[i] == ')' ){
+
+        for (int i = 0; i < s.length(); i++)
+        {
+
+            if (!st.empty() && st.top() == '(' && s[i] == ')')
+            {
                 st.pop();
             }
-            else if(!st.empty() && st.top() == '[' && s[i] == ']' ){
+            else if (!st.empty() && st.top() == '[' && s[i] == ']')
+            {
                 st.pop();
             }
-            else if(!st.empty() && st.top() == '{' && s[i] == '}' ){
+            else if (!st.empty() && st.top() == '{' && s[i] == '}')
+            {
                 st.pop();
-            }else{
+            }
+            else
+            {
                 st.push(s[i]);
             }
         }
-        if(st.size()>0) return false;
+        if (st.size() > 0)
+            return false;
         return true;
     }
 };
