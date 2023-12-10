11905612937d17cd02088a1b253dd178b3f4eff7
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-10 19:56:37 +0530

    write Node class

---
+++ Supreme 2.0/25-binarySearchTree/Lecture-1/01-creatingBST.cpp
@@ -0,0 +1,23 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data) : data(data), left(NULL), right(NULL) {}
+};
+
+
+int main()
+{
+
+    return 0;
+}
