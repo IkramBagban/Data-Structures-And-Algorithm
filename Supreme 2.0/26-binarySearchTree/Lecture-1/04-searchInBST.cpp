b56a86cf7605948bacb76154627fd9fcee381d00
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-14 14:44:18 +0530

    Search In BST (Leetcode)

--- Supreme 2.0/26-binarySearchTree/Lecture-1/04-searchInBST.cpp
+++ Supreme 2.0/26-binarySearchTree/Lecture-1/04-searchInBST.cpp
@@ -1,23 +1,29 @@
 // Author: Ikram Bagban
 // Email: bagbanikram@gmail.com
 // GitHub: https://github.com/ikrambagban
+// Question : https://leetcode.com/problems/search-in-a-binary-search-tree/
 
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-
+        // If the root is NULL, the tree is empty or we've reached the end of a branch.
         if(root == NULL){
             return NULL;
         }
         
+        // If the current node's value matches the search value, return the current node.
         if(root->val == val){
             return root;
         }
 
+        // If the search value is greater than the current node's value, 
+        // search in the right subtree.
         if(root->val < val){
-           return root->right = searchBST(root->right, val);
-        }else{
-           return root->left = searchBST(root->left, val);
+           return searchBST(root->right, val);
+        } else {
+            // If the search value is less than the current node's value, 
+            // search in the left subtree.
+           return searchBST(root->left, val);
         }  
     }
 };
