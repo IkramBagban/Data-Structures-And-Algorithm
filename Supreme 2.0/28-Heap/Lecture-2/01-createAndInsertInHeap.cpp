8133a9b993ae5c89a0c936d83bbe09968758bb53
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2023-12-16 15:10:28 +0530

    insert in heap

---
+++ Supreme 2.0/28-Heap/Lecture-2/01-createAndInsertInHeap.cpp
@@ -0,0 +1,51 @@
+// Author: Ikram Bagban
+// Email: bagbanikram@gmail.com
+// GitHub: https://github.com/ikrambagban
+
+#include <iostream>
+using namespace std;
+
+class Heap
+{
+public:
+    int *arr;
+    int size;
+    int capacity;
+
+    Heap(int capacity)
+    {
+        arr = new int[capacity];
+        this->size = 0;
+        this->capacity = capacity;
+    }
+
+    void insert(int val){
+        if(size == capacity){
+            cout << "Heap Overflow" << endl;
+            return;
+        }
+
+        size++;
+        int index = size;
+        arr[index] = val;
+
+        while(index > 1){
+            int parentIndex = index/2;
+            if(arr[parentIndex] > arr[index]) {
+                break;
+            }else{
+                swap(arr[parentIndex], arr[index]);
+            }
+
+
+        }
+
+
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
